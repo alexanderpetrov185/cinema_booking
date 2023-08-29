b3343a5daac0307e72ac5986b5aa394f6d1ff9ee
Alexander <alexander185@gmail.com>
2023-08-29 18:34:26 +0300

    created template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Gallery from "./pages/gallery/Gallery";
+import Prices from "./pages/prices/Prices";
+import Repertoire from "./pages/repertoire/Repertoire";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home/>,
+        },
+        {
+            path: "/about",
+            element: <About/>,
+        },
+        {
+            path: "/gallery",
+            element: <Gallery/>,
+        },
+        {
+            path: "/prices",
+            element: <Prices/>,
+        },
+        {
+            path: "/repertoire",
+            element: <Repertoire/>,
+        },
+    ]);
+
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
