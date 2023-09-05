78db40fc35dc5ebb6558b7f8a764b125f7e5d621
Alexander <alexander185@gmail.com>
2023-09-05 21:24:10 +0300

    finished slider for pc

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import "./slider.scss"
 import {ArrowBackIosOutlined, ArrowForwardIosOutlined} from "@mui/icons-material";
+import {Link} from 'react-router-dom';
 
 type Props = {
     slides: {
@@ -11,36 +12,54 @@ type Props = {
 
 const Slider = ({slides}: Props) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [isTransitionEnd, setTransition] = useState(true)
+
     const goToPrevious = () => {
         const isFirstSlide = currentIndex === 0;
         const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
+        isTransitionEnd && setCurrentIndex(newIndex);
+        setTransition(false)
     };
     const goToNext = () => {
         const isLastSlide = currentIndex === slides.length - 1;
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
+        isTransitionEnd && setCurrentIndex(newIndex);
+        setTransition(false)
     };
     const goToSlide = (slideIndex: number) => {
-        setCurrentIndex(slideIndex);
+        isTransitionEnd && setCurrentIndex(slideIndex);
+        setTransition(false)
     };
     const backgroundImg = {
         backgroundImage: `url(${slides[currentIndex].url})`,
     };
 
+    const handleTransitionEvent = () => {
+        setTransition(true)
+    }
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            goToNext();
+        }, 5000);
+
+        return () => clearInterval(timer);
+    });
+
 
     return (
         <div className="slider">
-            <div className="slideItem" style={backgroundImg}>
-                <ArrowBackIosOutlined
-                    className="sliderArrow left"
-                    onClick={goToPrevious}
-                />
-                <ArrowForwardIosOutlined
-                    className="sliderArrow right"
-                    onClick={goToNext}
-                />
-            </div>
+            <Link to={"/movieItem"}>
+                <div className="slideItem" style={backgroundImg} onTransitionEnd={handleTransitionEvent}/>
+            </Link>
+            <ArrowBackIosOutlined
+                className="sliderArrow left"
+                onClick={goToPrevious}
+            />
+            <ArrowForwardIosOutlined
+                className="sliderArrow right"
+                onClick={goToNext}
+            />
             <div className="sliderDots">
                 {slides.map((slide, slideIndex) => (
                     <div className={slideIndex === currentIndex ? "sliderDot Active" : "sliderDot"}
