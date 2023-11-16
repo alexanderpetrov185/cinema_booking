9517cc12b1c33b156234c8c67dfb8b50de6ad144
Alexander <alexander185@gmail.com>
2023-11-16 15:29:48 +0300

    optimizing and changing design

--- src/components/movieItem/MovieItem.tsx
+++ src/components/movieItem/MovieItem.tsx
@@ -1,47 +1,57 @@
-import React, {useState} from 'react';
-import "./movieItem.scss"
+import React, { useState } from "react";
+import "./movieItem.scss";
 import ModalWindow from "../modalWindow/ModalWindow";
 import BookingModule from "../bookingModule/BookingModule";
-import {IMovie} from "../../redux/models/IMovie";
-import Player from '../player/Player';
+import { IMovie } from "../../redux/models/IMovie";
+import Player from "../player/Player";
 import SessionCard from "../sessionDetails/SessionCard";
 
-
 type Props = {
-    movie: IMovie
-}
-
-const MovieItem = ({movie}: Props) => {
-    const [playerIsOpen, setPlayerIsOpen] = useState(false);
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const nowDate = new Date() // Дата и Время в нашем регионе (!На стороне сервера не учитывается часовой пояс)
-
-
-    return (
-        <div className="movieItem" key={movie.imdbID}>
-            <div className="imageContainer">
-                <img src={movie.poster} alt={`poster ${movie.title}`}
-                    // onClick={() => setModalIsOpen(true)}
-                />
-                <button className="playButton" onClick={() => setPlayerIsOpen(true)}>
-                    <span className="play"/>
-                </button>
-                <ModalWindow modalIsOpen={playerIsOpen} setModalIsOpen={setPlayerIsOpen}>
-                    {playerIsOpen && <Player videoSrc={movie.trailer}/>}
-                </ModalWindow>
-            </div>
-            <SessionCard title={movie.title} genre={movie.genre} sessionsDetails={movie.sessionsDetails}
-                         nowDate={nowDate} setModalIsOpen={setModalIsOpen}/>
-            <ModalWindow modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}>
-                {modalIsOpen &&
-                    <BookingModule nowDate={nowDate}
-                                   title={movie.title} details={movie.sessionsDetails}
-                                   setModalIsOpen={setModalIsOpen}/>}
-            </ModalWindow>
-        </div>
-    );
+  movie: IMovie;
 };
 
+const MovieItem = ({ movie }: Props) => {
+  const [playerIsOpen, setPlayerIsOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const nowDate = new Date(); // Дата и Время в нашем регионе (!На стороне сервера не учитывается часовой пояс)
+
+  return (
+    <div className="movieItem" key={movie.imdbID}>
+      <div className="imageContainer">
+        <img
+          src={movie.poster}
+          alt={`poster ${movie.title}`}
+          // onClick={() => setModalIsOpen(true)}
+        />
+        <button className="playButton" onClick={() => setPlayerIsOpen(true)}>
+          <span className="play" />
+        </button>
+        <ModalWindow
+          modalIsOpen={playerIsOpen}
+          setModalIsOpen={setPlayerIsOpen}
+        >
+          {playerIsOpen && <Player videoSrc={movie.trailer} />}
+        </ModalWindow>
+      </div>
+      <SessionCard
+        title={movie.title}
+        genre={movie.genre}
+        sessionsDetails={movie.sessionsDetails}
+        nowDate={nowDate}
+        setModalIsOpen={setModalIsOpen}
+      />
+      <ModalWindow modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen}>
+        {modalIsOpen && (
+          <BookingModule
+            nowDate={nowDate}
+            title={movie.title}
+            details={movie.sessionsDetails}
+            setModalIsOpen={setModalIsOpen}
+          />
+        )}
+      </ModalWindow>
+    </div>
+  );
+};
 
 export default MovieItem;
-
