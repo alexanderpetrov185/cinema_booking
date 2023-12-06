b28864b6a45cec954dbf2b66e77712dfda4b5f48
Alexander <alexander185@gmail.com>
2023-12-06 04:16:41 +0300

    pure reusable infinite slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,16 +1,12 @@
 import React, {
   memo,
   useCallback,
-  useLayoutEffect,
+  useEffect,
   useMemo,
   useRef,
   useState,
 } from "react";
 import "./slider.scss";
-import {
-  ArrowBackIosOutlined,
-  ArrowForwardIosOutlined,
-} from "@mui/icons-material";
 
 type Props = {
   slides: {
@@ -24,50 +20,67 @@ const Slider = memo(({ slides }: Props) => {
     () => [slides[slides.length - 1], ...slides, slides[0]],
     [slides],
   );
-
   const containerRef = useRef<HTMLDivElement | null>(null);
   const [currentIndex, setCurrentIndex] = useState(1);
   const [translateX, setTranslateX] = useState(0);
+  const [transitionState, setTransitionState] = useState(false);
+  const [isTransitionEnd, setIsTransitionEnd] = useState(true);
 
   const goToNext = useCallback(() => {
-    if (containerRef.current && currentIndex >= 1) {
-      containerRef.current.style.transitionDuration = "400ms";
-    }
-    if (currentIndex !== slidesToRender.length - 1)
-      setCurrentIndex((state) => (state + 1) % slidesToRender.length);
+    setIsTransitionEnd(false);
+    if (currentIndex >= 1) setTransitionState(true); //duplicate code
+    setCurrentIndex((state) => (state + 1) % slidesToRender.length);
   }, [currentIndex, slidesToRender.length]);
 
   const goToPrevious = useCallback(() => {
-    if (containerRef.current && currentIndex <= slidesToRender.length - 2) {
-      containerRef.current.style.transitionDuration = "400ms";
-    }
-    if (currentIndex !== 0)
-      setCurrentIndex((state) => (state - 1) % slidesToRender.length);
+    setIsTransitionEnd(false);
+    if (currentIndex <= slidesToRender.length - 2) setTransitionState(true); //duplicate code
+    setCurrentIndex((state) => (state - 1) % slidesToRender.length);
   }, [currentIndex, slidesToRender.length]);
 
-  const getSlidesContainerStyle = useCallback(() => {
-    return {
-      transform: `translateX(${-translateX}px)`,
-    };
-  }, [translateX]);
+  const goToSlide = useCallback(
+    (slideIndex: number) => {
+      if (currentIndex >= 1) setTransitionState(true); //duplicate code
+      if (currentIndex <= slidesToRender.length - 2) setTransitionState(true); //duplicate code
+      setIsTransitionEnd(false);
+      setCurrentIndex(slideIndex);
+    },
+    [currentIndex, slidesToRender.length],
+  );
 
-  useLayoutEffect(() => {
+  const getSlidesContainerStyle = useCallback(
+    () => ({
+      transform: `translateX(${-translateX}px)`,
+      transition: `${transitionState ? "transform 0.5s ease-in-out" : false}`,
+    }),
+    [transitionState, translateX],
+  );
+
+  const handleTransition = () => {
+    setIsTransitionEnd(true);
+    if (currentIndex === 0) {
+      setTransitionState(false);
+      setCurrentIndex(slidesToRender.length - 2);
+    }
+    if (currentIndex === slidesToRender.length - 1) {
+      setTransitionState(false);
+      setCurrentIndex(1);
+    }
+  };
+
+  useEffect(() => {
     if (containerRef.current) {
       setTranslateX(containerRef.current.clientWidth * currentIndex);
     }
   }, [currentIndex]);
 
-  const handleTransition = () => {
-    if (containerRef.current && currentIndex === 0) {
-      containerRef.current.style.transitionDuration = "0ms";
-      setCurrentIndex(slidesToRender.length - 2);
-    }
-
-    if (containerRef.current && currentIndex === slidesToRender.length - 1) {
-      containerRef.current.style.transitionDuration = "0ms";
-      setCurrentIndex(1);
-    }
-  };
+  //auto-slide
+  useEffect(() => {
+    const timer = setInterval(() => {
+      goToNext();
+    }, 5000);
+    return () => clearInterval(timer);
+  });
 
   return (
     <div className="slider">
@@ -87,22 +100,33 @@ const Slider = memo(({ slides }: Props) => {
           ></div>
         ))}
       </div>
-      <ArrowBackIosOutlined
+      <button
         className="sliderArrow left"
         onClick={goToPrevious}
-      />
-      <ArrowForwardIosOutlined
+        disabled={!isTransitionEnd}
+      >
+        {"<"}
+      </button>
+      <button
         className="sliderArrow right"
         onClick={goToNext}
-      />
+        disabled={!isTransitionEnd}
+      >
+        {">"}
+      </button>
       <div className="sliderDots">
         {slides.map((slide, slideIndex) => (
           <div
             className={
-              // slideIndex === currentIndex ? "sliderDot Active" :
-              "sliderDot"
+              slideIndex + 1 === currentIndex || //if the number of the current slide is equal to the currentIndex
+              (slideIndex === 0 && //if we switch from the last slide to the first
+                currentIndex === slidesToRender.length - 1) ||
+              (slideIndex === slides.length - 1 && currentIndex === 0) //if we switch from the first slide to the last
+                ? "sliderDot Active"
+                : "sliderDot"
             }
             key={slideIndex}
+            onClick={() => goToSlide(slideIndex + 1)}
           >
             ●
           </div>
